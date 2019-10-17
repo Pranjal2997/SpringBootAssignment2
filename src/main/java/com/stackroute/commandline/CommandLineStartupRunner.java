165cdc8b8a726dbd3250d481406eb6d5fdcb6cd8
pranjal.sarin <pranjal.sarin@cgi.com>
2019-10-17 12:58:03 +0530

    1st commit

---
+++ src/main/java/com/stackroute/commandline/CommandLineStartupRunner.java
@@ -0,0 +1,21 @@
+package com.stackroute.commandline;
+
+import com.stackroute.domain.Track;
+import com.stackroute.repository.TrackRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommandLineStartupRunner implements CommandLineRunner {
+    @Autowired
+    private TrackRepository trackRepository;
+
+    public void setTrackRepository(TrackRepository trackRepository) {
+        this.trackRepository = trackRepository;
+    }
+    @Override
+    public void run(String... args) throws Exception {
+        trackRepository.save(new Track(3, "Emoji of a Wave", "John Mayer" ,"Oh honey, it's just a wave" ));
+    }
+}
